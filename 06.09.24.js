c4073a80734c576f3fb208b88b78698c0c514b72
Nadia <148998471+NadiaSuganiaka@users.noreply.github.com>
2024-09-06 22:59:27 +0300

    exponentiation
    I had to exponentiation a number to the n power through the action of multiplication.

---
+++ 06.09.24.js
@@ -0,0 +1,46 @@
+
+let x = prompt("Enter the value x: "); //variable
+
+    x = Number(x); //text to number
+
+            let a = x * x; //2
+            let b = a * a; //4
+            let c = b * b; //8
+            let d = c * c; //16
+            let e = d * d; //32
+            let f = e * e; //64
+            let g = x * a; //3
+            let h = g * a; //5
+            let i = h * h; //10
+            let j = i * h; //15
+            let k = i * i; //20
+            let l = d * x; //17
+            let m = k * x; //21
+            let n = i * g; //13
+            let o = k * c; //28
+
+        let one = c; let two = l; let three = i; 
+        let four = n; let five = m; let six = j;
+        let seven = o; let eight = f; let nine = k;
+ 
+    //output
+    console.log("first", one);
+
+    console.log("second", two);
+
+    console.log("third", three);
+
+    console.log("fourth", four);
+
+    console.log("fifth", five);
+
+    console.log("sixth", six);
+
+    console.log("seventh", seven);
+
+    console.log("eighth", eight);
+
+    console.log("ninth", nine);
+
+        
+       
